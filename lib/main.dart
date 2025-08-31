27d444cee4b32defb6f02d4ae46da3af6d0006d4
dedejuniarputra99 <dedejuniarputra99@gmail.com>
2025-08-31 19:33:09 +0000

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('PRAKTIKUM MOBILE LANJUT'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hallo Nama Saya DEDE JUNIAR PUTRA',
+            textAlign: TextAlign.center,
+            // style: TextStyle(fontSize: 16), // opsional
+          ),
+        ),
+      ),
+    );
+  }
+}
